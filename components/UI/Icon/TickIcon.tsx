9e416eb7c53dda05d5b2247811ae6a58dcc31418
nikolas-tuz <tuznikolas@gmail.com>
2025-05-31 15:54:06 +0300

    Removed - symbol from all svg attrs in TickIcon.tsx component to fix react warnings;

--- components/UI/Icon/TickIcon.tsx
+++ components/UI/Icon/TickIcon.tsx
@@ -12,8 +12,8 @@ export default function TickIcon({ width, height, strokeColor }: TickIconType) {
     <>
       <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox={`0 0 ${width} ${height}`}
            fill="none">
-        <path d="M2.75 8.75L6.25 12.25L13.25 4.75" stroke={strokeColor} stroke-width="1.5" stroke-linecap="round"
-              stroke-linejoin="round" />
+        <path d="M2.75 8.75L6.25 12.25L13.25 4.75" stroke={strokeColor} strokeWidth="1.5" strokeLinecap="round"
+              strokeLinejoin="round" />
       </svg>
     </>
   );
