de5b913ebd9cfd5c612f73fc1817be89e691e054
nikolas-tuz <tuznikolas@gmail.com>
2025-05-31 13:17:50 +0300

    Removed - symbol from all svg attrs in ChevronIcon.tsx component to fix react warnings;

--- components/UI/Icon/ChevronIcon.tsx
+++ components/UI/Icon/ChevronIcon.tsx
@@ -11,8 +11,8 @@ export default function ChevronIcon({ stroke = `#3E3E3E`, ...props }: ChevronIco
   return (
     <>
       <svg {...props} xmlns="http://www.w3.org/2000/svg" width="13" height="9" viewBox="0 0 9 5" fill="none">
-        <path d="M1.07422 0.815918L4.69107 4.43277L8.30791 0.815918" stroke={stroke} stroke-width="1.00468"
-              stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M1.07422 0.815918L4.69107 4.43277L8.30791 0.815918" stroke={stroke} strokeWidth="1.00468"
+              strokeLinecap="round" strokeLinejoin="round" />
       </svg>
     </>
   );
