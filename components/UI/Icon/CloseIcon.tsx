7226a9ac6cd16de7709d364e019a3db4b5b2c076
nikolas-tuz <tuznikolas@gmail.com>
2025-05-31 13:18:34 +0300

    Removed - symbol from all svg attrs in CloseIcon.tsx component to fix react warnings;

--- components/UI/Icon/CloseIcon.tsx
+++ components/UI/Icon/CloseIcon.tsx
@@ -20,7 +20,7 @@ export default function
       {variant === `default` && (
         <>
           <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none">
-            <path d="M20 20L4 4M20 4L4 20" stroke={strokeColor} stroke-width="2" stroke-linecap="round" />
+            <path d="M20 20L4 4M20 4L4 20" stroke={strokeColor} strokeWidth="2" strokeLinecap="round" />
           </svg>
         </>
       )}
